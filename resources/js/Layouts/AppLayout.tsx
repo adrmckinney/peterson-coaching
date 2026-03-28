7ee025bb439b36767b713537a4652b3b6c53fab2
Dan McKinney <adrmckinney@gmail.com>
2026-03-28 15:45:02 -0400

    added fallback text and removed call to backend for text

--- resources/js/Layouts/AppLayout.tsx
+++ resources/js/Layouts/AppLayout.tsx
@@ -1,45 +1,10 @@
-import { SectionProvider } from "@/ContextProviders/SectionProvider";
 import { ThemeProvider } from "@/ContextProviders/ThemeProvider";
-import { PageProps } from "@/types";
-import { Page } from "@/types/Page";
-import { PageSection } from "@/types/PageSections";
-import { usePage } from "@inertiajs/react";
 import { PropsWithChildren } from "react";
 
 export default function AppLayout({ children }: PropsWithChildren) {
-    const { flash, pages, sections, isEditable } = usePage<
-        PageProps<{
-            pages: Page[];
-            sections: PageSection[];
-            isEditable: boolean;
-            flash?: {
-                success?: string;
-                error?: string;
-            };
-        }>
-    >().props;
-
     return (
         <ThemeProvider>
-            {/* <ConditionalRender condition={!!flash?.success || !!flash?.error}>
-                <div
-                    className={[
-                        "absolute mb-6 rounded-md px-4 py-3",
-                        flash?.success
-                            ? "bg-green-600/20 border border-green-600 text-green-200"
-                            : "bg-red-600/20 border border-red-600 text-red-200",
-                    ].join(" ")}
-                >
-                    {flash?.success}
-                </div>
-            </ConditionalRender> */}
-            <SectionProvider
-                pages={pages}
-                sections={sections}
-                isEditable={isEditable}
-            >
-                {children}
-            </SectionProvider>
+            {children}
         </ThemeProvider>
     );
 }
--- resources/js/actions/App/Http/Controllers/LandingController.ts
+++ resources/js/actions/App/Http/Controllers/LandingController.ts
@@ -1,7 +1,7 @@
-import { queryParams, type RouteQueryOptions, type RouteDefinition, applyUrlDefaults } from './../../../../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition } from './../../../../wayfinder'
 /**
 * @see \App\Http\Controllers\LandingController::index
-* @see app/Http/Controllers/LandingController.php:12
+* @see app/Http/Controllers/LandingController.php:9
 * @route '/landing'
 */
 export const index = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
@@ -16,7 +16,7 @@ index.definition = {
 
 /**
 * @see \App\Http\Controllers\LandingController::index
-* @see app/Http/Controllers/LandingController.php:12
+* @see app/Http/Controllers/LandingController.php:9
 * @route '/landing'
 */
 index.url = (options?: RouteQueryOptions) => {
@@ -25,7 +25,7 @@ index.url = (options?: RouteQueryOptions) => {
 
 /**
 * @see \App\Http\Controllers\LandingController::index
-* @see app/Http/Controllers/LandingController.php:12
+* @see app/Http/Controllers/LandingController.php:9
 * @route '/landing'
 */
 index.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
@@ -35,7 +35,7 @@ index.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
 
 /**
 * @see \App\Http\Controllers\LandingController::index
-* @see app/Http/Controllers/LandingController.php:12
+* @see app/Http/Controllers/LandingController.php:9
 * @route '/landing'
 */
 index.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
@@ -43,105 +43,6 @@ index.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
     method: 'head',
 })
 
-/**
-* @see \App\Http\Controllers\LandingController::edit
-* @see app/Http/Controllers/LandingController.php:46
-* @route '/admin/landing'
-*/
-export const edit = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
-    url: edit.url(options),
-    method: 'get',
-})
-
-edit.definition = {
-    methods: ["get","head"],
-    url: '/admin/landing',
-} satisfies RouteDefinition<["get","head"]>
-
-/**
-* @see \App\Http\Controllers\LandingController::edit
-* @see app/Http/Controllers/LandingController.php:46
-* @route '/admin/landing'
-*/
-edit.url = (options?: RouteQueryOptions) => {
-    return edit.definition.url + queryParams(options)
-}
-
-/**
-* @see \App\Http\Controllers\LandingController::edit
-* @see app/Http/Controllers/LandingController.php:46
-* @route '/admin/landing'
-*/
-edit.get = (options?: RouteQueryOptions): RouteDefinition<'get'> => ({
-    url: edit.url(options),
-    method: 'get',
-})
-
-/**
-* @see \App\Http\Controllers\LandingController::edit
-* @see app/Http/Controllers/LandingController.php:46
-* @route '/admin/landing'
-*/
-edit.head = (options?: RouteQueryOptions): RouteDefinition<'head'> => ({
-    url: edit.url(options),
-    method: 'head',
-})
-
-/**
-* @see \App\Http\Controllers\LandingController::patchSection
-* @see app/Http/Controllers/LandingController.php:80
-* @route '/admin/pages/{page}/sections/{section}'
-*/
-export const patchSection = (args: { page: number | { id: number }, section: number | { id: number } } | [page: number | { id: number }, section: number | { id: number } ], options?: RouteQueryOptions): RouteDefinition<'patch'> => ({
-    url: patchSection.url(args, options),
-    method: 'patch',
-})
-
-patchSection.definition = {
-    methods: ["patch"],
-    url: '/admin/pages/{page}/sections/{section}',
-} satisfies RouteDefinition<["patch"]>
-
-/**
-* @see \App\Http\Controllers\LandingController::patchSection
-* @see app/Http/Controllers/LandingController.php:80
-* @route '/admin/pages/{page}/sections/{section}'
-*/
-patchSection.url = (args: { page: number | { id: number }, section: number | { id: number } } | [page: number | { id: number }, section: number | { id: number } ], options?: RouteQueryOptions) => {
-    if (Array.isArray(args)) {
-        args = {
-            page: args[0],
-            section: args[1],
-        }
-    }
-
-    args = applyUrlDefaults(args)
-
-    const parsedArgs = {
-        page: typeof args.page === 'object'
-        ? args.page.id
-        : args.page,
-        section: typeof args.section === 'object'
-        ? args.section.id
-        : args.section,
-    }
-
-    return patchSection.definition.url
-            .replace('{page}', parsedArgs.page.toString())
-            .replace('{section}', parsedArgs.section.toString())
-            .replace(/\/+$/, '') + queryParams(options)
-}
-
-/**
-* @see \App\Http\Controllers\LandingController::patchSection
-* @see app/Http/Controllers/LandingController.php:80
-* @route '/admin/pages/{page}/sections/{section}'
-*/
-patchSection.patch = (args: { page: number | { id: number }, section: number | { id: number } } | [page: number | { id: number }, section: number | { id: number } ], options?: RouteQueryOptions): RouteDefinition<'patch'> => ({
-    url: patchSection.url(args, options),
-    method: 'patch',
-})
-
-const LandingController = { index, edit, patchSection }
+const LandingController = { index }
 
 export default LandingController
